ea2aa7b9f2cbcb142a06bde6b8d5e3277ba883ad
Gwendolyn <wilkistagwendolyn@gmail.com>
2025-05-28 14:55:49 +0300

    Navigation between pages

---
+++ alx-project-0x01/pages/posts/index.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/layout/Header';
+
+const Post: React.FC = () => {
+  return(
+    <div className='flex flex-col h-screen'>
+      <Header/>
+      
+    </div>
+  )
+}
+export default Post;
---
+++ alx-project-0x01/pages/users/index.tsx
@@ -0,0 +1,11 @@
+import Header from '@/components/layout/Header';
+
+const User: React.FC = () => {
+  return(
+    <div className='flex flex-col h-screen'>
+      <Header/>
+      
+    </div>
+  )
+}
+export default User;
